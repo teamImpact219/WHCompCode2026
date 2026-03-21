2a27ffc02b0385eb3d8c527ed89c1c6f446daeb0
MAX <maxkielzeroeight@gmail.com>
2026-03-21 15:01:01 -0400

    blind turn to tag done scoreee

--- src/main/java/frc/robot/commands/fieldRelativeTurnToTagCommand.java
+++ src/main/java/frc/robot/commands/fieldRelativeTurnToTagCommand.java
@@ -6,8 +6,20 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.UniCamVisionSubsystem;
 //requester imports
 import com.ctre.phoenix6.swerve.SwerveRequest;
+//position imports
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+//util imports
+import java.util.Optional;
+import java.lang.Math;
+import edu.wpi.first.math.MathUtil;
 
-//WIP
+
+/*
+ * NOTE: RELIES ENTIRELY ON THE ABILITY TO DETERMINE CURRENT FIELD POS, 
+ * PLEASE MAKE SURE TO EITHER MAINTAIN AN ACCURATE POSITION OR
+ * COMPENSATE FOR THIS RISK
+*/
 public class fieldRelativeTurnToTagCommand extends Command {
   //variables
     //drivetrain variables
@@ -15,6 +27,11 @@ public class fieldRelativeTurnToTagCommand extends Command {
     SwerveRequest.FieldCentric driveCommand;
     //tag variables
     int tagID;
+    boolean tagExists = false;
+    //position variables
+    Pose2d botPosition;
+    Pose2d tagPosition;
+    double error;
   public fieldRelativeTurnToTagCommand(CommandSwerveDrivetrain drivetrain,SwerveRequest.FieldCentric driveCommand, int id) {
     //reqs
     this.drivetrain = drivetrain;
@@ -25,11 +42,38 @@ public class fieldRelativeTurnToTagCommand extends Command {
   }
 
   @Override
-  public void initialize() {}
+  public void initialize() {
+    Optional<Pose3d> optPosContainer = UniCamVisionSubsystem.apriltagPositions.getTagPose(tagID);
+    if (optPosContainer.isPresent()){
+      tagPosition = optPosContainer.get().toPose2d();
+      tagExists = true;
+    }
+
+  }
   @Override
-  public void execute() {}
+  public void execute() {
+    if (tagExists){
+      //gets the current bot position
+      botPosition = drivetrain.getState().Pose;
+      //finds the error in the angles between the tag and bot positions (rad)
+      error = 
+      Math.atan2(
+        tagPosition.getY()-botPosition.getY(),
+        tagPosition.getX()-botPosition.getX()
+      ) - botPosition.getRotation().getRadians();
+      //normalizes to [-pi, pi]
+      error = MathUtil.angleModulus(error);
+      //actually rotating to it
+      drivetrain.setControl(driveCommand.withRotationalRate(-0.5*error)); 
+    }
+  }
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+      drivetrain.setControl(driveCommand.withRotationalRate(0)); 
+
+
+  }
+  
   @Override
-  public boolean isFinished() {return false;}
+  public boolean isFinished() {return !tagExists || Math.abs(error) < Math.PI/180.0;}
 }
